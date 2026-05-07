d9709a18a7c1a22d21b245fe2fec5cf2d4af72ae
Ashwini Hombal <ashwinihombal02@gmail.com>
2026-05-07 17:20:12 +0530

    Delete StudioProjects directory

--- StudioProjects/fintrack_pro/lib/l10n/language_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final languageProvider = StateProvider<Locale>((ref) {
-  return const Locale('en'); // default language
-});
